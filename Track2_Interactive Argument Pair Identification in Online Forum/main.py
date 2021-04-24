1403117e626db1a728dce68c3f3380e98b28274f
AIDebater <82932750+AIDebater@users.noreply.github.com>
2021-04-25 04:33:28 +0800

    Add files via upload

---
+++ Track2_Interactive Argument Pair Identification in Online Forum/main.py
@@ -0,0 +1,56 @@
+import json
+import os
+
+from utils import get_path
+import torch
+import pandas as pd
+from torch.utils.data import DataLoader
+
+from data import CMVDataset
+from utils import create_mini_batch, load_torch_model, get_logits, compute_acc, get_answers
+from model import BaselineModel
+from transformers import BertTokenizer
+
+
+def main(config_file='config.json'):
+    """Main method for training.
+    Args:
+        config_file: in config dir
+    """
+    # 0. Load config and mkdir
+    with open(config_file) as fin:
+        config = json.load(fin)
+    get_path(os.path.join(config['model_path'], config['experiment_name']))
+    get_path(config['log_path'])
+
+    # 1. Load data
+    tokenizer = BertTokenizer.from_pretrained(config['model_version'])
+    # train_set = CMVDataset(path="train.txt", mode='train', tokenizer=tokenizer)
+    # train_loader = DataLoader(train_set, batch_size=10, shuffle=True, collate_fn=create_mini_batch)
+    data_set = CMVDataset(path=config['test_file_path'], mode='test', tokenizer=tokenizer)
+    data_loader = DataLoader(dataset=data_set, batch_size=1, shuffle=False, collate_fn=create_mini_batch)
+
+    # 2. Load model
+    model = BaselineModel(config)
+    # model = load_torch_model(
+    #     model, model_path=os.path.join(config.model_path, 'model.bin'))
+    model.load_state_dict(torch.load(os.path.join(config['model_path'], 'model.bin')))
+    device = torch.device('cuda:2' if torch.cuda.is_available() else 'cpu')
+    model.to(device)
+
+    # 3. Evaluate
+    logits = get_logits(data_loader=data_loader, tokenizer=tokenizer, device=device, model=model)
+    label = [1] * len(pd.read_csv(config['test_file_path'], header=None, sep='#'))
+    acc = compute_acc(label=label, logits=logits)
+    print(f"Model's Acc:{acc}")
+
+    # 4. Output submission file
+    answers = get_answers(logits)
+    with open(config['output_file'], 'w') as fout:
+        fout.write('id,answer\n')
+        for i, j in enumerate(answers):
+            fout.write(str(i) + ',' + str(j) + '\n')
+
+
+if __name__ == '__main__':
+    main()
